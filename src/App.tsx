0a61497ddd271c1c6373544b41a9e7951fa27c13
duanzin <lucaduan@gmail.com>
2023-09-28 13:28:35 -0300

    Project started

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>CoreNotes</h1>
+        <input type="search" placeholder="Pesquisar notas"/>
+      </header>
+      <main>
+        <form>
+          <input type="text" placeholder="Título" />
+          <input type="text" placeholder="Criar nota..." />
+        </form>
+      </main>
+    </>
+  );
+}
+
+export default App;
